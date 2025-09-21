2b4232b76e3c1d92e41df8c5614d38e075221d40
lezob <theo.cimino@gmail.com>
2025-09-21 23:43:25 +0200

    pas grand chose de nouveau ici

---
+++ build/CMakeFiles/MonProjet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MonProjet.dir/src/test.cpp.obj"
+  "CMakeFiles/MonProjet.dir/src/test.cpp.obj.d"
+  "MonProjet.exe"
+  "MonProjet.exe.manifest"
+  "MonProjet.pdb"
+  "libMonProjet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MonProjet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MonProjet.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MonProjet.
+# This may be replaced when dependencies are built.
